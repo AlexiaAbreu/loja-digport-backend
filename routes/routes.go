53287df0365d0dffcd743c53c2e2eb066f94d20c
Alexia Abreu <alexia28.abreu@gmail.com>
2024-08-01 14:30:07 -0300

    retomada das aulas

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import{
+	"net/http"
+
+	"github.com/gorilla/mux"
+}
+
+func HandleRequests(){
+	route := mux.NewRouter()
+
+	route.HandleFunc("/procutos", controller.BuscaProdutosHandler).Methods("GET")
+	route.HandleFunc("/procuto", controller.BuscaProdutosPorNomeHandler).Methods("GET")
+	route.HandleFunc("/procutos", controller.CriaProdutosHandler).Methods("POST")
+
+	http.ListenAndServe(":8080", route)
+
+}
---
+++ server.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/AlexiaAbreu/loja-digport-backend/tree/main/routes"
+
+func StartServer() {
+	routes.HandleRequests()
+}
