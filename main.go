05b5b83c73a9135f339cd3c6f0b3b3322d4a0407
Alexia Abreu <alexia28.abreu@gmail.com>
2024-09-12 13:36:45 -0300

    aula implementação frontend

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/AlexiaAbreu/loja-digport-backend/db"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	db.InitDB()
 	StartServer()
 
 }
--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/AlexiaAbreu/loja-digport-backend/controller"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func HandleRequests() {
@@ -14,8 +15,18 @@ func HandleRequests() {
 	route.HandleFunc("/produto", controller.BuscaProdutoPorNomeHandler).Methods("GET")
 	route.HandleFunc("/produto", controller.CriaProdutoHandler).Methods("POST")
 	route.HandleFunc("/produto/{id}", controller.RemoveProdutoHandler).Methods("DELETE")
+	// route.HandleFunc("/produto/{id}", controller.BuscaProdutoPorIdHandler).Methods("GET")
 	//route.HandleFunc("/produtos", controller.AtualizaProdutoHandler).Methods("PUT")
 
-	http.ListenAndServe(":8080", route)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "DELETE", "PUT"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(route)
+
+	http.ListenAndServe(":8080", handler)
 
 }
